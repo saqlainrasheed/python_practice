c2ae96ed5c7bc4f3fd80944f003f7bc9c1526e25
M. Saqlain Rasheed <msaqi681@gmail.com>
2021-07-11 20:30:36 +0500

    Solved the indentation problem

--- merge_data_together/db.py
+++ merge_data_together/db.py
@@ -10,40 +10,40 @@ def create_table(connection):
     cursor = connection.cursor()
     cursor.execute(
         """ 
-		CREATE TABLE user ( 
-		id INTEGER PRIMARY KEY,
-		email VARCHAR(255),
-		name VARCHAR(255),
-		phone VARCHAR(18),
-		username VARCHAR(255),
-		website VARCHAR(255)
-		) 
-		"""
+        CREATE TABLE user ( 
+        id INTEGER PRIMARY KEY,
+        email VARCHAR(255),
+        name VARCHAR(255),
+        phone VARCHAR(18),
+        username VARCHAR(255),
+        website VARCHAR(255)
+        ) 
+        """
     )
 
     cursor.execute(
         """
-		CREATE TABLE post (
-		id INTEGER PRIMARY KEY,
-		body VARCHAR(1000),
-		title VARCHAR(255),
-		user_id INTEGER ,
-		FOREIGN KEY(user_id) REFERENCES user(id)
-		)
-		"""
+        CREATE TABLE post (
+        id INTEGER PRIMARY KEY,
+        body VARCHAR(1000),
+        title VARCHAR(255),
+        user_id INTEGER ,
+        FOREIGN KEY(user_id) REFERENCES user(id)
+        )
+        """
     )
 
     cursor.execute(
         """
-		CREATE TABLE comment (
-		id INTEGER PRIMARY KEY,
-		post_id INTEGER ,
-		name VARCHAR(255),
-		email VARCHAR(255),
-		body VARCHAR(255),
-		FOREIGN KEY(post_id) REFERENCES post(id)
-		)
-		"""
+        CREATE TABLE comment (
+        id INTEGER PRIMARY KEY,
+        post_id INTEGER ,
+        name VARCHAR(255),
+        email VARCHAR(255),
+        body VARCHAR(255),
+        FOREIGN KEY(post_id) REFERENCES post(id)
+        )
+        """
     )
     connection.commit()
 
@@ -52,12 +52,12 @@ def insert_user(cursor, users):
     for user in users:
         cursor.execute(
             """
-			INSERT INTO user (
-				id, email, name, phone, username, website
-			) VALUES (
-				?, ?, ?, ?, ?, ?
-			)
-     	 	""", [
+            INSERT INTO user (
+                id, email, name, phone, username, website
+            ) VALUES (
+                ?, ?, ?, ?, ?, ?
+            )
+              """, [
                 user['id'],
                 user['email'],
                 user['name'],
@@ -72,12 +72,12 @@ def insert_post(cursor, posts):
     for post in posts:
         cursor.execute(
             """
-			INSERT INTO post (
-				id, body, title, user_id
-			) VALUES (
-				?, ?, ?, ?
-			)
-      		""", [
+            INSERT INTO post (
+                id, body, title, user_id
+            ) VALUES (
+                ?, ?, ?, ?
+            )
+              """, [
                 post['id'],
                 post['body'],
                 post['title'],
@@ -90,12 +90,12 @@ def insert_comment(cursor, comments):
     for comment in comments:
         cursor.execute(
             """
-			INSERT INTO comment (
-				id, body, email, name, post_id
-			) VALUES (
-				?, ?, ?, ?, ?
-			)
-      		""", [
+            INSERT INTO comment (
+                id, body, email, name, post_id
+            ) VALUES (
+                ?, ?, ?, ?, ?
+            )
+              """, [
                 comment['id'],
                 comment['body'],
                 comment['email'],
